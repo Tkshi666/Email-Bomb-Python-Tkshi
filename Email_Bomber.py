192e2da11ae3b2e8e4901c511709262cd8375fff
Tkshi666 <52582186+Tkshi666@users.noreply.github.com>
2019-10-21 11:47:59 +0200

    Update Email_Bomber.py

--- Email_Bomber.py
+++ Email_Bomber.py
@@ -11,7 +11,7 @@ class bcolors:
 
 def banner():
     print(bcolors.GREEN + '+[+[+[ Email-Bomber v1.0 ]+]+]+')
-    print(bcolors.GREEN + '+[+[+[ made with codes ]+]+]+')
+    print(bcolors.GREEN + '+[+[+[ Email Bomber coder par Tkshi de la Orbital ! ]+]+]+')
     print(bcolors.GREEN + '''
                      \|/
                        `--+--'
